74808674e09f3427c61cefd4931ff84fa9bcdd12
sirebellum <joshua.herrera227@gmail.com>
2018-03-30 19:49:09 -0500

    Better script behavior

--- startup.sh
+++ startup.sh
@@ -3,8 +3,8 @@
 export DISPLAY=:0
 sleep 15
 
-unclutter -idle 0 #hide mouse
+unclutter -idle 0 & #hide mouse
 
 #Start chromium into campaign url
 campaign_url="http://jupiter.signage.me/WebService/SignagePlayerApp.html?eri=f7bee07a7e79c8f1d7951b4d24de4713c22f16&playerParams=14789f72800b050f9ab8a92fedd219788cccdf44"
-chromium-browser --kiosk "$campaign_url"
+chromium-browser --kiosk --incognito "$campaign_url" &
